78aea608b837bbaf4b641be997b404d0d328b406
subodh <subodh.shukla95@gmail.com>
2019-05-29 16:58:13 +0530

    Initial commit

---
+++ math.java
@@ -0,0 +1,21 @@
+/**
+* This is test class for GIT
+*/
+public class math{
+	public void add(int a, int b){
+		System.out.println(int c=a+b);
+	}
+	public void sub(int a, int b){
+		System.out.println(int c=a-b);
+	}
+	public void mult(int a, int b){
+		System.out.println(int c=a*b);
+	}
+	public static void main(Strings arg[]){
+		math obj=new math();
+		obj.add(8,9);
+		obj.sub(8,9);
+		obj.mult(8,9);
+	}
+	
+}
