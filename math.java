22c96206bf8eaf5e4581edd65672d3357fdbbf29
subodh <subodh.shukla95@gmail.com>
2019-05-29 17:03:30 +0530

    changed in class file

--- math.java
+++ math.java
@@ -1,5 +1,6 @@
 /**
 * This is test class for GIT
+* Verifing the git functionality
 */
 public class math{
 	public void add(int a, int b){
@@ -11,11 +12,15 @@ public class math{
 	public void mult(int a, int b){
 		System.out.println(int c=a*b);
 	}
+	public void ave(int a, int b){
+		System.out.println(int c=(a+b)/2)
+	}
 	public static void main(Strings arg[]){
 		math obj=new math();
 		obj.add(8,9);
 		obj.sub(8,9);
 		obj.mult(8,9);
+		obj.ave(8,9);
 	}
 	
 }
