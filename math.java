9af9dd38c6c3af0aee58f1a6ea15d255d0813efc
subodh <subodh.shukla95@gmail.com>
2019-05-30 17:32:02 +0530

    removed average function

--- math.java
+++ math.java
@@ -13,15 +13,13 @@ public class math{
 	public void mult(int a, int b){
 		System.out.println(int c=a*b);
 	}
-	public void ave(int a, int b){
-		System.out.println(int c=(a+b)/2)
-	}
+
 	public static void main(Strings arg[]){
 		math obj=new math();
 		obj.add(8,9);
 		obj.sub(8,9);
 		obj.mult(8,9);
-		obj.ave(8,9);
+
 	}
 	//Test for branch1
 }
